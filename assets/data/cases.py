fb6fac12d34055811d4fe185c8da0bf6efb7d83d
malrashdi23 <malrashdi99@gmail.com>
2023-09-12 23:19:12 +0300

    add cases

---
+++ assets/data/cases.py
@@ -0,0 +1,41 @@
+import sys
+
+NO_PATH = sys.maxsize
+
+sample_a = [[0, 5, NO_PATH, 10],
+            [NO_PATH, 0, 3, NO_PATH],
+            [NO_PATH, NO_PATH, 0,   1],
+            [NO_PATH, NO_PATH, NO_PATH, 0]]
+
+sample_b = [[0, NO_PATH, 9, NO_PATH],
+            [NO_PATH, 0, NO_PATH, NO_PATH],
+            [NO_PATH, -5, 0, 8],
+            [-3, 2, NO_PATH, 0]]
+
+
+
+sample_d = [[0, 8, NO_PATH, NO_PATH, 5, 3, NO_PATH, NO_PATH],
+            [NO_PATH, 0, NO_PATH, 11, 5, NO_PATH, 9, NO_PATH],
+            [7, 4, 0, NO_PATH, NO_PATH, 4, NO_PATH, NO_PATH],
+            [NO_PATH, NO_PATH, NO_PATH, 0, 1, NO_PATH, NO_PATH, 2],
+            [NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, NO_PATH, NO_PATH, NO_PATH],
+            [NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, NO_PATH, NO_PATH],
+            [NO_PATH, NO_PATH, NO_PATH, 3, NO_PATH, NO_PATH, 0, 12],
+            [NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0]]
+
+sample_e = [[0, 8, NO_PATH, NO_PATH, 5, 3, NO_PATH, NO_PATH, 5, 2, 8, 11, 23, 17, 1, 2],
+            [NO_PATH, 0, NO_PATH, 11, 5, NO_PATH, 9, NO_PATH, NO_PATH, 4, NO_PATH, 1, 4, 11, NO_PATH, 8],
+            [7, 4, 0, NO_PATH, NO_PATH, 4, NO_PATH, NO_PATH, 5, 3, NO_PATH, NO_PATH, 5, 2, 8, 11],
+            [4, 4, 8, 0, 1, NO_PATH, NO_PATH, 2, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 3],
+            [4, 4, 8, NO_PATH, 0, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 4, NO_PATH, 11, 5, NO_PATH, 9, NO_PATH],
+            [4, 4, 8, NO_PATH, NO_PATH, 0, NO_PATH, NO_PATH, 7, 4, 9, NO_PATH, NO_PATH, 4, NO_PATH, NO_PATH],
+            [4, 4, 8, 3, NO_PATH, 2, 0, 12, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 12],
+            [4, 4, 8, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, 5, 2, 9, 22, 3, 7, 9, 1],
+            [2, 8, 8, NO_PATH, 5, 3, NO_PATH, NO_PATH, 0, 2, 8, 11, 23, 17, 1, 2],
+            [NO_PATH, 5, NO_PATH, 11, 5, NO_PATH, 9, NO_PATH, NO_PATH, 0, NO_PATH, 1, 4, 11, NO_PATH, 8],
+            [7, 4, 2, NO_PATH, NO_PATH, 4, NO_PATH, NO_PATH, 5, 3, 0, NO_PATH, 5, 2, 8, 11],
+            [NO_PATH, 7, 4, NO_PATH, 1, 2, 9, 2, NO_PATH, NO_PATH, NO_PATH, 0, NO_PATH, NO_PATH, NO_PATH, 3],
+            [NO_PATH, 7, 4, 11, 2, NO_PATH, NO_PATH, 3, 5, 4, NO_PATH, 11, 0, NO_PATH, 9, NO_PATH],
+            [NO_PATH, 7, 4, NO_PATH, NO_PATH, 9, NO_PATH, NO_PATH, 7, 4, 9, NO_PATH, NO_PATH, 0, NO_PATH, NO_PATH],
+            [NO_PATH, 7, 4, 3, NO_PATH, NO_PATH, 8, 12, NO_PATH, 6, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, 12],
+            [NO_PATH, 7, 4, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 1, 5, 2, NO_PATH, 22, 3, 7, 9, 0]]
